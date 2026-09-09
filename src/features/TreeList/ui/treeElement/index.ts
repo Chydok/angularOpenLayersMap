9b124ab2981a571fa749d48382e9fb1b914fa365
chydok <chydok@gmail.com>
2026-09-09 20:26:15 +0300

    add catalog icon in treeElement

--- src/features/TreeList/ui/treeElement/index.ts
+++ src/features/TreeList/ui/treeElement/index.ts
@@ -3,9 +3,14 @@ import { Component, model } from "@angular/core";
 @Component({
 	selector: "TreeElement",
 	template: `
-		<span>{{name()}}</span>
+		<div style="display: flex; align-items: center; height: 100%; gap: 0.25rem;">
+			@if (isExpanded()) {<img src="assets/icons/folderOpen.svg" alt="Иконка" [width]="16" [height]="16" />}
+			@else {<img src="assets/icons/folder.svg" alt="Иконка" [width]="16" [height]="16" />}
+			<span>{{name()}}</span>
+		</div>
 	`
 })
 export class TreeElement {
-	name = model("")
+	name = model<string>("")
+	isExpanded = model<boolean>(false)
 }
